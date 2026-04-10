3ad6983a955dd867bcbfa6e98843e9c0876760f4
xav98 <hpaulo406@gmail.com>
2026-04-10 18:04:49 -0300

    a

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.slick').slick({
-      slidesToShow: 3,
+      slidesToShow: 2,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 1200,
+      autoplaySpeed: 2000,
     });
   });
